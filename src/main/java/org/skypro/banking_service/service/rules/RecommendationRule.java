0de4a299502113bf20c2d1b8f2d881a15e6a4dda
vachg <vachgen@yandex.ru>
2025-05-01 12:56:31 +0300

    branchVG 1

--- src/main/java/org/skypro/banking_service/service/rules/RecommendationRule.java
+++ src/main/java/org/skypro/banking_service/service/rules/RecommendationRule.java
@@ -1,7 +1,12 @@
 package org.skypro.banking_service.service.rules;
 
 
+import org.skypro.banking_service.dto.RecommendationDto;
+
+import java.util.Optional;
+
 public interface RecommendationRule {
 
+    Optional<RecommendationDto> checkOut(String userId);
 }
 
