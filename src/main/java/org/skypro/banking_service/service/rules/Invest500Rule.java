fa3b563daad1af07d5f96e5f7e716b952c705c86
evgen <evgeny15q@gmail.com>
2025-05-01 13:58:18 +0700

    creating Invest500Rule with returning Optional

--- src/main/java/org/skypro/banking_service/service/rules/Invest500Rule.java
+++ src/main/java/org/skypro/banking_service/service/rules/Invest500Rule.java
@@ -1,8 +1,10 @@
 package org.skypro.banking_service.service.rules;
 
+import org.skypro.banking_service.dto.RecommendationDto;
 import org.skypro.banking_service.repository.RecommendationRepository;
 import org.springframework.stereotype.Component;
 
+import java.util.Optional;
 import java.util.UUID;
 
 @Component
@@ -14,12 +16,21 @@ public class Invest500Rule implements RecommendationRule {
         this.recommendationRepository = recommendationRepository;
     }
 
-    public Boolean getProduct(UUID userId) {
+    public Optional<RecommendationDto> checkOut(UUID userId) {
+        RecommendationDto Invest500 = new RecommendationDto(
+                "147f6a0f-3b91-413b-ab99-87f081d60d5a",
+                "Invest 500",
+                "Откройте свой путь к успеху с индивидуальным инвестиционным счетом (ИИС) от нашего банка! Воспользуйтесь налоговыми льготами и начните инвестировать с умом. Пополните счет до конца года и получите выгоду в виде вычета на взнос в следующем налоговом периоде. Не упустите возможность разнообразить свой портфель, снизить риски и следить за актуальными рыночными тенденциями. Откройте ИИС сегодня и станьте ближе к финансовой независимости!"
+        );
+
         boolean requirementFirst = recommendationRepository.existsUserProductByType(userId, "DEBIT");
         boolean requirementSecond = !(recommendationRepository.existsUserProductByType(userId, "INVEST"));
         boolean requirementThird = recommendationRepository.findTotalDepositByUserIdAndProductType(userId, "SAVING") > 1000;
 
-        return requirementFirst && requirementSecond && requirementThird;
+        if (requirementFirst && requirementSecond && requirementThird) {
+            return Optional.of(Invest500);
+        }
+        return Optional.empty();
     }
 
 }
