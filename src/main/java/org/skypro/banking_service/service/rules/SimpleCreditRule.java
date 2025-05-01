d6eab7cb37fc95615d6d1b21c9e3c5c7f9f95289
Maria_Prokhorova <luchiksolnza@gmail.com>
2025-05-01 22:00:50 +0700

    create SimpleCreditRule

--- src/main/java/org/skypro/banking_service/service/rules/SimpleCreditRule.java
+++ src/main/java/org/skypro/banking_service/service/rules/SimpleCreditRule.java
@@ -1,9 +1,28 @@
 package org.skypro.banking_service.service.rules;
 
+import org.skypro.banking_service.repository.RecommendationRepository;
 import org.springframework.stereotype.Component;
+
+import java.util.*;
 
 @Component
 public class SimpleCreditRule implements RecommendationRule {
+    private final RecommendationRepository repository;
 
+
+    public SimpleCreditRule(RecommendationRepository repository) {
+        this.repository = repository;
+    }
+
+    @Override
+    public boolean checkOut(UUID userId) {
+        Set<String> products = new HashSet<>(repository.findUsedProductTypesByUserId(userId));
+        long deposit = repository.findTotalDepositByUserIdAndProductType(userId, "DEBIT");
+        long withdraw = repository.findTotalWithdrawByUserIdAndProductType(userId, "DEBIT");
+
+        if (!products.contains("CREDIT") && deposit > withdraw && withdraw > 100_000) {
+            return true;
+        } else return false;
+    }
 }
 
