396347d4aadf0dc33ec713ef12727765d66b0e56
evgen <evgeny15q@gmail.com>
2025-05-03 10:07:15 +0700

    Testing services

---
+++ src/test/java/org/skypro/banking_service/serviceTest/RulesServiceTest.java
@@ -0,0 +1,68 @@
+package org.skypro.banking_service.serviceTest;
+
+import org.junit.jupiter.api.Test;
+import org.skypro.banking_service.repository.RecommendationRepository;
+import org.skypro.banking_service.service.RulesService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.UUID;
+
+import static org.skypro.banking_service.service.constants.ProductConstants.*;
+import static org.assertj.core.api.Assertions.*;
+
+@SpringBootTest
+public class RulesServiceTest {
+
+    @Autowired
+    private RulesService rulesService;
+
+
+    @Test
+    void shouldReturnTrueIfUserIsUsingProductDebit() {
+        UUID userId = UUID.fromString("cd515076-5d8a-44be-930e-8d4fcb79f42d");
+        assertThat(rulesService.isUsingProduct(
+                userId,
+                TYPE_DEBIT)).isTrue();
+    }
+
+    @Test
+    void shouldReturnTrueIfAmountDepositMoreLimit() {
+        UUID userId = UUID.fromString("cd515076-5d8a-44be-930e-8d4fcb79f42d");
+
+        assertThat(rulesService.isAmountDepositMoreLimit(
+                userId,
+                TYPE_SAVING,
+                LIMIT_INVEST_500)).isTrue();
+    }
+
+    @Test
+    void shouldReturnTrueIfAmountSeveralDepositsMoreOrEqualsLimit() {
+        UUID userId = UUID.fromString("1f9b149c-6577-448a-bc94-16bea229b71a");
+
+        assertThat(rulesService.isAmountSeveralDepositsMoreOrEqualsLimit(
+                userId,
+                TYPE_DEBIT,
+                TYPE_SAVING,
+                LIMIT_TOP_SAVING)).isTrue();
+     }
+
+    @Test
+    void shouldReturnTrueIfAmountWithdrawMoreLimit() {
+        UUID userId = UUID.fromString("1f9b149c-6577-448a-bc94-16bea229b71a");
+
+        assertThat(rulesService.isAmountWithdrawMoreLimit(
+                userId,
+                TYPE_DEBIT,
+                LIMIT_SIMPLE_CREDIT)).isTrue();
+    }
+
+    @Test
+    void shouldReturnTrueIfAmountDepositsMoreThanWithdrawals() {
+        UUID userId = UUID.fromString("1f9b149c-6577-448a-bc94-16bea229b71a");
+
+        assertThat(rulesService.isAmountDepositsMoreThanWithdrawals(
+                userId,
+                TYPE_DEBIT)).isTrue();
+    }
+}
---
+++ src/test/java/org/skypro/banking_service/serviceTest/RulesTest.java
@@ -0,0 +1,71 @@
+package org.skypro.banking_service.serviceTest;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.skypro.banking_service.dto.RecommendationDto;
+import org.skypro.banking_service.service.rules.Invest500Rule;
+import org.skypro.banking_service.service.rules.SimpleCreditRule;
+import org.skypro.banking_service.service.rules.TopSavingRule;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.Optional;
+import java.util.UUID;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.skypro.banking_service.service.constants.ProductConstants.*;
+
+@SpringBootTest
+public class RulesTest {
+
+    @Autowired
+    Invest500Rule invest500Rule;
+
+    @Autowired
+    TopSavingRule topSavingRule;
+
+    @Autowired
+    SimpleCreditRule simpleCreditRule;
+
+
+    @Test
+    void shouldReturnRecommendationInvest500() {
+        UUID userId = UUID.fromString("cd515076-5d8a-44be-930e-8d4fcb79f42d");
+
+        Optional<RecommendationDto> result = invest500Rule.checkOut(userId);
+        assertEquals(new RecommendationDto(
+                PRODUCT_NAME_INVEST_500,
+                PRODUCT_ID_INVEST_500,
+                DESCRIPTION_INVEST_500), result.get());
+    }
+
+    @Test
+    void shouldReturnRecommendationTopSaving() {
+        UUID userId = UUID.fromString("d4a4d619-9a0c-4fc5-b0cb-76c49409546b");
+
+        Optional<RecommendationDto> result = topSavingRule.checkOut(userId);
+        assertEquals(new RecommendationDto(
+                PRODUCT_NAME_TOP_SAVING,
+                PRODUCT_ID_TOP_SAVING,
+                DESCRIPTION_TOP_SAVING), result.get());
+    }
+
+    @Test
+    void shouldReturnRecommendationSimpleCredit() {
+        UUID userId = UUID.fromString("1f9b149c-6577-448a-bc94-16bea229b71a");
+
+        Optional<RecommendationDto> result = simpleCreditRule.checkOut(userId);
+        assertEquals(new RecommendationDto(
+                PRODUCT_NAME_SIMPLE_CREDIT,
+                PRODUCT_ID_SIMPLE_CREDIT,
+                DESCRIPTION_SIMPLE_CREDIT), result.get());
+    }
+
+    @Test
+    void shouldReturnEmptyRecommendation() {
+        UUID userId = UUID.fromString("1f9b149c-6577-448a-bc94-16bea229b71a");
+
+        Optional<RecommendationDto> result = invest500Rule.checkOut(userId);
+        Assertions.assertTrue(result.isEmpty());
+    }
+}
